c254f6169d98fa5132bea97ee12131a158f89a32
Ruan Aragão <ruanaragao2@gmail.com>
2023-12-12 23:42:06 -0300

    🧪 test: add unit test for WindowComponent

---
+++ components/window-component.unit.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest'
+import { mount } from '@vue/test-utils'
+import WindowComponent from './window-component.vue'
+
+const findByRef = (wrapper: any, ref: string) => wrapper.find({ ref })
+
+test('shold render window component', () => {
+  const props = {
+    title: 'Window' as string,
+    onClose: () => {},
+  }
+
+  const wrapper = mount(WindowComponent, { props })
+
+  const windowRef = findByRef(wrapper, 'windowRef')
+
+  expect(windowRef.exists()).toBe(true)
+  expect(wrapper.find('header').text()).toBe(props.title)
+  expect(wrapper.find('button.close').exists()).toBe(true)
+  expect(wrapper.find('button.maximize').exists()).toBe(true)
+  expect(wrapper.find('button.minimize').exists()).toBe(true)
+})
