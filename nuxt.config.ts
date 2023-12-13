ccea9876f8dce689affcb3e29e0c1f56d8044ab2
Ruan Aragão <ruanaragao2@gmail.com>
2023-12-12 22:14:21 -0300

    chore: add vitest configuration and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/tailwindcss',
-  ],
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', "nuxt-vitest"],
 
   routeRules: {
     '/**': {
