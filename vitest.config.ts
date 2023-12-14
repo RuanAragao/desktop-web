3bcae5f107c5cc48ea7d72114c94c8f2d68e6d7e
Ruan Aragão <ruanaragao2@gmail.com>
2023-12-13 22:27:36 -0300

    fix: add vite-tsconfig-paths plugin to vite.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [vue() as any],
+  plugins: [vue() as any, tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
+    alias: {
+      '~/': new URL('./', import.meta.url).pathname,
+      '@/': new URL('./', import.meta.url).pathname,
+    },
   },
 })
