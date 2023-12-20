1661f2aedb3d663c8ee04921e4045db1957cd381
Ruan Aragão <ruanaragao2@gmail.com>
2023-12-20 02:47:12 -0300

    feat: add xterm.js and disable app

--- applications/index.ts
+++ applications/index.ts
@@ -1,8 +1,8 @@
 import type { Application } from '../types'
 
 import about from './about/settings'
-import xterm from './xterm/settings'
+// import xterm from './xterm/settings'
 
 export default function Applications() {
-  return [about, xterm] as Application[]
+  return [about] as Application[]
 }
--- applications/xterm/index.vue
+++ applications/xterm/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <input v-model="command" @keydown.enter="executeCommand" />
-    <pre>{{ output }}</pre>
-  </div>
+  <div id="terminal"></div>
 </template>
 
-<script>
+<script lang="ts">
+import { Terminal } from 'xterm'
+import 'xterm/css/xterm.css'
+
 export default {
   name: 'XTerm',
   data() {
@@ -14,13 +14,30 @@ export default {
       output: '',
     }
   },
-  methods: {
-    executeCommand() {
-      // Execute the command and update the output
-      // You can replace this with your own logic
-      this.output += `$ ${this.command}\n`
-      this.command = ''
-    },
+
+  mounted() {
+    const term = new Terminal({
+      cursorBlink: true,
+      rows: 30,
+      cols: 100,
+      theme: {
+        background: '#000',
+        foreground: '#fff',
+      },
+    })
+
+    term.open(document.getElementById('terminal') as HTMLElement)
+
+    term.write('ghost \x1B[1;3;31mxterm\x1B[0m $ ')
   },
+
+  methods: {},
 }
 </script>
+
+<style scoped>
+#terminal {
+  height: 100%;
+  width: 100%;
+}
+</style>
