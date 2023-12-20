98a6b3ed5b74d90dafaa3216bfa5581a9a9bdc9a
Ruan Aragão <ruanaragao2@gmail.com>
2023-12-20 02:54:38 -0300

    fix: remove xterm

--- applications/index.ts
+++ applications/index.ts
@@ -1,7 +1,6 @@
 import type { Application } from '../types'
 
 import about from './about/settings'
-// import xterm from './xterm/settings'
 
 export default function Applications() {
   return [about] as Application[]
